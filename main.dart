802d08e9c72380d97cb3cdb44cb7d0f543772a5a
gustavo-gd <gustavo.diasf@uni9.edu.br>
2025-09-03 23:59:42 +0000

    feat:  if statement

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+void main(){
+    int idade = 18;
+    if (idade >= 18)
+    {
+        print('pode entrar na balada');
+        return;
+    }
+    print(' fim do programa');
+}
